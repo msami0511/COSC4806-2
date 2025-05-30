b7b55f89f6e7917bac7e04d76353df7a67e586da
msami0511 <mohamashaikh@algomau.ca>
2025-05-30 00:46:02 +0000

    Conifg page added for  database

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php 
+
+define('DB_HOST',  'q3jzh.h.filess.io');
+define('DB_USER',  'COSC4806_earsnowsit');
+define('DB_PASS',  $_ENV['DB_PASS']);
+define ('DB_DATABASE', 'COSC4806_earsnowsit');
+define ('DB_PORT',  '61000');
+
+  ?>
